36b82754260e8dd0df471f5da7dc3b7992d16927
vikash <vikashvs36@gmail.com>
2019-06-30 03:04:23 +0530

    Create new table on Annotation

--- src/com/Demo.java
+++ src/com/Demo.java
@@ -1,6 +1,7 @@
 package com;
 
 import com.modal.User;
+import com.modal.UserDetails;
 import org.hibernate.*;
 
 import java.util.Scanner;
@@ -9,17 +10,17 @@ public class Demo {
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        System.out.print("Enter EmpName: ");
-        String n=in.nextLine();
+        System.out.print("Enter Name: ");
+        String name=in.nextLine();
         in.close();
         //com.modal.User object is created
-        User e=new User(n);
+        User user=new User(name);
         System.out.println("Persiting com.modal.User object...");
         Session session=MyFactory.getSession();
         //Transaction is get started from the session
         Transaction t=session.beginTransaction();
         //Session is asked to save the entity
-        session.save(e);
+        session.save(user);
         //transaction is get committed
         t.commit();
         //session is closed
